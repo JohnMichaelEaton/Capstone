aa57f670a41c2f3f1a92e4c159cef3eab7966ec0
John Michael Eaton <jeaton1@harding.edu>
2018-01-15 13:46:29 -0600

    Beginnings of the Project
    Beginning files added.
    Repository created.

---
+++ Enumerations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Capstone
+{
+    enum PlayerType
+    {
+        Local = 0,
+        Network = 1,
+        AI = 2,
+    }
+
+    enum PlayerColor
+    {
+        White = 0,
+        Black = 1,
+    }
+}
